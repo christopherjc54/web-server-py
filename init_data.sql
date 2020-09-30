f491bf9f4f9ff17da83ca7126977e7bd8d5e5da4
Christopher Coutinho <christopherjc54@users.noreply.github.com>
2020-09-29 21:02:05 -0500

    updated database schema

--- init_data.sql
+++ init_data.sql
@@ -1,9 +1,11 @@
 -- add sample data
 
 USE `DatabaseServer`;
-INSERT INTO Account VALUES(
-    "sampleuser",                                                           -- username
-    "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",     -- password hash (password is "password")
-    "8cc36E0a5A5b04A8",                                                     -- salt
+
+INSERT INTO Account (`username`, `displayName`, `passwordHash`, `salt`, `hash`) VALUES (
+    "sampleuser",
+    "Bob Smith",
+    "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",     -- SHA-256 hash of "password"
+    "8cc36E0a5A5b04A8",
     "43ea5f3e6d4b9f71bd478cf49e8f3c7df6b0fdd45d25637ef2fba73e8df431e4"      -- salted and hashed password
 );
--- init_db.sql
+++ init_db.sql
@@ -1,13 +1,53 @@
--- configure db for first time after logging in as 'user'
+-- configure db for first time after logging in as "user"
 
-DROP DATABASE IF EXISTS `DatabaseServer`;
+DROP DATABASE IF EXISTS `DatabaseServer`;   -- use either "drop database" or most "drop tables"
 CREATE DATABASE `DatabaseServer`;
 USE `DatabaseServer`;
+
 DROP TABLE IF EXISTS `Account`;
 CREATE TABLE `Account` (
+    `id` INT NOT NULL AUTO_INCREMENT,       -- might not be used
     `username` VARCHAR(15),
-    `passwordHash` VARCHAR(256),    -- do not use in production
-    `salt` VARCHAR(16),             -- 16 hex digits => 64 bits
-    `hash` VARCHAR(256),            -- SHA-256
-    PRIMARY KEY (`username`)
+    `displayName` VARCHAR(32),
+    `passwordHash` VARCHAR(256),            -- do not use in production
+    `salt` VARCHAR(16),                     -- 16 char * 4 bits for hex digits => 64 bits
+    `hash` VARCHAR(256),                    -- SHA-256
+    PRIMARY KEY (`username`),
+    UNIQUE (`id`, `username`, `displayName`)
 );
+
+DROP TABLE IF EXISTS `Session`;
+CREATE TABLE `Session` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(15),
+    `sessionID` VARCHAR(32),                -- 32 char * 4 bits for hex digits => 128 bits
+    `expDateTime` DATETIME,
+    PRIMARY KEY (`username`),
+    FOREIGN KEY (`username`) REFERENCES Account(`username`),
+    UNIQUE (`id`)
+);
+
+DROP TABLE IF EXISTS `Message`;
+CREATE TABLE `Message` (                    -- doesn't have foreign keys on usernames because messages still exist after user is deleted
+                                            -- should probably check for orphan messages when deleting a user (also prevents creating new account to view old messages)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `fromUsername` VARCHAR(15),
+    `toUsername` VARCHAR(15),
+    `message` VARCHAR(512),
+    `sentDateTime` DATETIME,
+    PRIMARY KEY (`toUsername`),
+    UNIQUE (`id`)
+);
+
+DROP PROCEDURE IF EXISTS `CleanMessages`;
+DELIMITER $$ ;
+CREATE PROCEDURE `CleanMessages`()
+BEGIN
+    DELETE m
+    FROM Message m
+    LEFT JOIN Account a
+    ON m.fromUsername = a.username
+    OR m.toUsername = a.username
+    WHERE a.username IS NULL;
+END$$
+DELIMITER ; $$
